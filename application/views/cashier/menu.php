144ca8a2efb1a6c785bb4f42b9aed90b8f5710b5
ngurahmantara <ngurahmantara@gmail.com>
2013-08-05 06:24:44 +0800

    fix menu

--- application/views/cashier/menu.php
+++ application/views/cashier/menu.php
@@ -4,4 +4,5 @@
 <?php echo anchor('cashier/payment','NPJG BARU', 'class="btn btn-default"'); ?>
 <?php echo anchor('harian/incoming','LPKH Incoming', 'class="btn btn-default"'); ?>
 <?php echo anchor('harian/outgoing','LPKH Outgoing', 'class="btn btn-default"'); ?>
+<?php echo anchor('cashier/payment/summary','Summary Report', 'class="btn btn-default"'); ?>
 </div>
